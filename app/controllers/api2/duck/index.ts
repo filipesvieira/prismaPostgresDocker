8855d42da9fcc8cc8094d85501f43302febb0543
Filipe <filipe@methodostecnologia.com.br>
2022-06-03 13:36:12 -0300

    Criação da segunda Api e requestBody

--- app/controllers/api2/duck/index.ts
+++ app/controllers/api2/duck/index.ts
@@ -3,21 +3,13 @@ import { PrismaClient } from "@prisma/client";
 /**
 * @swagger
 * components:
-*
 *   schemas:
-*
-*     jogadorData:
+*     patoData:
 *         type: array
 *         items:
 *           type: object
 *           properties:
 *             nome:
-*               type: string
-*               required: true
-*             posicao:
-*               type: string
-*               required: true
-*             altura:
 *               type: string
 *               required: true
 *             peso:
@@ -30,47 +22,45 @@ interface App {
     get: any;
 }
 
-interface Jogador {
+interface Pato {
     nome?: string,
-    posicao?: string,
-    altura?: string,
     peso?: string,
 };
 
-let jogadores: Array<Jogador> = [];
+let patos: Array<Pato> = [];
 
 module.exports = (app: App) => {
     /**
     * @swagger
-    * /jogador:
+    * /duck:
     *   post:
-    *     summary: Inserta novos jogadores
-    *     description: Endpoint que recebe array de objetos com os dados de jogadores que serão inseridos na base de dados
+    *     summary: Inserta novos patos
+    *     description: Endpoint que recebe array de objetos com os dados de patos que serão inseridos na base de dados
     *     tags:
-    *       - JOGADOR
+    *       - PATO DUCK
     *     security:
     *       - bearerAuth: []
     *     produces:
     *       - application/json
     *     requestBody:
-    *         description: "Array de objetos para insertar novos jogadores"
+    *         description: "Array de objetos para insertar novos patos"
     *         required: true
     *         content:
     *           application/json:
     *             schema:
-    *               $ref: "#/components/schemas/jogadorData"
+    *               $ref: "#/components/schemas/patoData"
     *     responses:
     *       200:
     *         description: "success"
     *       400:
     *         description: "bad request"
     */
-    app.post('/jogador', async (req: any, res: any) => {
+    app.post('/duck', async (req: any, res: any) => {
         let prisma: PrismaClient;
         prisma = new PrismaClient();
         if (req.method === 'POST') {
             try {
-                let jogadores = await prisma.jogador.createMany({
+                let jogadores = await prisma.pato.createMany({
                     data: req.body,
                     skipDuplicates: true,
                 });
@@ -88,12 +78,12 @@ module.exports = (app: App) => {
 
     /**
     * @swagger
-    * /jogador:
+    * /duck:
     *   get:
-    *     summary: Busca dados dos jogadores
-    *     description: Busca dados dos jogadores já cadastrados na base de dados de acordo com os parâmetros enviados
+    *     summary: Busca dados dos patos
+    *     description: Busca dados dos patos já cadastrados na base de dados de acordo com os parâmetros enviados
     *     tags:
-    *       - JOGADOR
+    *       - PATO DUCK
     *     security:
     *       - bearerAuth: []
     *     produces:
@@ -101,12 +91,12 @@ module.exports = (app: App) => {
     *     parameters:
     *       - name: nome
     *         in: query
-    *         description: "Nome do jogador"
+    *         description: "Nome do duck"
     *         required: false
     *         type: string
     *       - name: posicao
     *         in: query
-    *         description: "Posição do jogador"
+    *         description: "Posição do duck"
     *         required: false
     *         type: string
     *     responses:
@@ -115,12 +105,12 @@ module.exports = (app: App) => {
     *       400:
     *         description: "bad request"
     */
-    app.get('/jogador', async (req: any, res: any) => {
+    app.get('/duck', async (req: any, res: any) => {
         let prisma: PrismaClient;
         prisma = new PrismaClient();
         if (req.method === 'GET') {
             try {
-                let jogadores = await prisma.jogador.findMany();
+                let jogadores = await prisma.pato.findMany();
                 res.status(200).json(JSON.parse(JSON.stringify(jogadores)));
             } catch (error) {
                 res.status(404).json(JSON.parse(JSON.stringify(error)));
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,6 +3,7 @@ const appRoot = require('app-root-path');
 const walkSync = require('walk-sync');
 
 module.exports = (app) => {
+    
     const controllers = walkSync(`${appRoot}/app/controllers/api`, {
         globs : ['**/*.ts'],
         ignore: ['index.ts', '**/lib/*.ts']
@@ -11,4 +12,14 @@ module.exports = (app) => {
 		console.log("TCL: Path", Path)
         require(`${appRoot}/app/controllers/api/${Path}`)(app);
     });
+
+    const controllers2 = walkSync(`${appRoot}/app/controllers/api2`, {
+        globs : ['**/*.ts'],
+        ignore: ['index.ts', '**/lib/*.ts']
+    });
+    controllers2.forEach( ( Path ) => {
+		console.log("TCL: Path", Path)
+        require(`${appRoot}/app/controllers/api2/${Path}`)(app);
+    });
+
 };
